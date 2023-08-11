a406c621cad8b80098d7d6803104243e27c6cf5d
Can US <me@canus.dev>
2023-08-11 07:29:42 +0300

    change core package

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.20
 
 require (
 	github.com/antandros/go-pkgparser v1.0.2 // indirect
+	github.com/knqyf263/go-rpmdb v0.0.0-20230723082926-067d98befa60 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- rpminfo.go
+++ rpminfo.go
@@ -1,107 +1,25 @@
 package rpminfo
 
 import (
-	"database/sql"
 	"errors"
 	"fmt"
 
 	"github.com/antandros/go-pkgparser"
 	"github.com/antandros/go-pkgparser/model"
-	_ "github.com/mattn/go-sqlite3"
+	rpmdb "github.com/knqyf263/go-rpmdb/pkg"
 )
 
-type PrimaryDatabase struct {
-	db     *sql.DB
-	dbpath string
-}
-
-func OpenPrimaryDB(path string) (*PrimaryDatabase, error) {
-	// open database file
-	db, err := sql.Open("sqlite3", path)
+func Packages() ([]*rpmdb.PackageInfo, error) {
+	db, err := rpmdb.Open("/var/lib/rpm/rpmdb.sqlite")
 	if err != nil {
 		return nil, err
 	}
-
-	// TODO: Validate primary_db on open, maybe with the db_info table
-
-	return &PrimaryDatabase{
-		db:     db,
-		dbpath: path,
-	}, nil
-}
-
-type PackageEntrySize struct {
-	Package   int64 `xml:"type,attr"`
-	Installed int64 `xml:"installed,attr"`
-	Archive   int64 `xml:"archive,attr"`
-}
-type PackageEntryVersion struct {
-	Epoch   int    `xml:"epoch,attr"`
-	Version string `xml:"ver,attr"`
-	Release string `xml:"rel,attr"`
-}
-type PackageEntryLocation struct {
-	Href string `xml:"href,attr"`
-}
-type PackageEntry struct {
-	db *PrimaryDatabase
-
-	Key         int
-	Arch        string               `xml:"arch"`
-	Size        PackageEntrySize     `xml:"size"`
-	Location    PackageEntryLocation `xml:"location"`
-	PackageName string               `xml:"name"`
-	Versions    PackageEntryVersion  `xml:"version"`
-	Summary     string               `xml:"summary"`
-	Url         string               `xml:"url"`
-	License     string               `xml:"rpm_license"`
-	Packager    string               `xml:"packager"`
-	Vendor      string               `xml:"vendor"`
-}
-type PackageEntries []PackageEntry
-
-const sqlSelectPackages = `SELECT
- pkgKey
- , name
- , arch
- , epoch
- , version
- , release
- , size_package
- , size_installed
- , size_archive
- , location_href
- , rpm_license
- , rpm_vendor
-FROM packages;`
-
-func Packages() (PackageEntries, error) {
-	c, err := OpenPrimaryDB("/var/lib/rpm/rpmdb.sqlite")
+	pkgList, err := db.ListPackages()
 	if err != nil {
 		return nil, err
 	}
-	rows, err := c.db.Query(sqlSelectPackages)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
+	return pkgList, nil
 
-	// parse each row as a package
-	packages := make(PackageEntries, 0)
-	for rows.Next() {
-		p := PackageEntry{
-			db: c,
-		}
-
-		// scan the values into the slice
-		if err = rows.Scan(&p.Key, &p.PackageName, &p.Arch, &p.Versions.Epoch, &p.Versions.Version, &p.Versions.Release, &p.Size.Package, &p.Size.Installed, &p.Size.Archive, &p.Location.Href, &p.License, &p.Vendor); err != nil {
-			return nil, fmt.Errorf("error scanning packages: %v", err)
-		}
-
-		packages = append(packages, p)
-	}
-
-	return packages, nil
 }
 func Parse(p *pkgparser.Parser) error {
 	packages, err := Packages()
@@ -113,16 +31,16 @@ func Parse(p *pkgparser.Parser) error {
 		packageItem := p.CreateModel()
 
 		mapItems := map[string]interface{}{
-			"Package":        rpmPackage.PackageName,
-			"Version":        rpmPackage.Versions.Version,
-			"Description":    rpmPackage.Summary,
-			"Installed-Size": rpmPackage.Size.Installed,
-			"Architecture":   rpmPackage.Arch,
-			"License":        rpmPackage.License,
-			"Homepage":       rpmPackage.Url,
-			"Revision":       rpmPackage.Versions.Release,
-			"Status":         "installed",
-			"Vendor":         rpmPackage.Vendor,
+			"Package":         rpmPackage.Name,
+			"Version":         rpmPackage.Version,
+			"Description":     rpmPackage.Summary,
+			"Installed-Size":  rpmPackage.Size,
+			"Architecture":    rpmPackage.Arch,
+			"License":         rpmPackage.License,
+			"Release":         rpmPackage.Release,
+			"Modularitylabel": rpmPackage.Modularitylabel,
+			"Status":          "installed",
+			"Vendor":          rpmPackage.Vendor,
 		}
 		for key, valn := range mapItems {
 			packageItem, err = p.SetValue(key, fmt.Sprintf("%v", valn), packageItem)
